9f41dc95a74ca8e4472e7f168f75ca6ee4ba2c5c
midas <dev@null>
2025-06-01 21:19:42 +0200

    add prometheus metrics

--- simplex-alerter/simplex_alerter/api/__init__.py
+++ simplex-alerter/simplex_alerter/api/__init__.py
@@ -1,7 +1,8 @@
 import aiohttp
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from observlib import traced, get_trace
 from aiohttp_socks import ProxyConnector
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request,Response
 from fastapi.responses import JSONResponse
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
@@ -27,7 +28,7 @@ def set_endpoint(endpoint):
 
 
 @app.on_event("startup")
-@traced
+@traced(timed = True)
 async def startup_event():
     span = get_trace().get_current_span()
     if local:
@@ -40,9 +41,14 @@ async def startup_event():
 
 
 @app.on_event("shutdown")
-@traced
+@traced(timed = True)
 async def shutdown_event():
     await app.state.aiohttp_session.close()
+
+@traced(timed = True
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 
 FastAPIInstrumentor().instrument_app(app)
