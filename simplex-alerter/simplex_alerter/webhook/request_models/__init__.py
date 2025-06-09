27368d0f9e9601ddb1e58e7f31ddbdab987c15a9
midas <dev@null>
2025-06-09 19:15:46 +0200

    wip testing jinja based alert rendering

--- simplex-alerter/simplex_alerter/webhook/request_models/__init__.py
+++ simplex-alerter/simplex_alerter/webhook/request_models/__init__.py
@@ -1,4 +1,5 @@
 from typing import Union
 from .grafana import GrafanaAlert
+from .servarr import ServarrAlert
 
-KnownModels = Union[GrafanaAlert]
+KnownModels = Union[GrafanaAlert, ServarrAlert]
--- simplex-alerter/simplex_alerter/webhook/request_models/servarr.py
+++ simplex-alerter/simplex_alerter/webhook/request_models/servarr.py
@@ -1,7 +1,35 @@
+from jinja2 import Environment
 from pydantic import BaseModel
 from typing import Union
 
-ServarrAlert = Union[Prowlarr]
 
-class ProwlarrAlert(BaseModel):
-    pass
+class SonarrAlert(BaseModel):
+    downloadClient: str
+    eventType: str
+    instanceName: str
+    applicationUrl: str
+    customFormatInfo: dict
+    downloadClient: str
+    downloadClientType: str
+    downloadId: str
+    release: dict
+    episodes: list
+    series: dict
+
+    def __init__(self):
+        self.template = """
+{eventType}
+
+{seriesTitle}:
+
+{% for ep in episodes %}
+- S{{ep.seasonNumber}}E{{ep.episodeNumber}}
+{%endfor%}
+"""
+
+    def render(self):
+        template = Environment.from_string(self.template)
+        return template.render(self.__dict__)
+
+
+ServarrAlert = Union[SonarrAlert]
