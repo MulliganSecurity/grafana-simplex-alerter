73bd0637c82affcae7865b2646259d042ad110e7
midas <dev@null>
2025-06-10 15:01:11 +0200

    add push notification for forgejo

--- simplex-alerter/simplex_alerter/webhook/request_models/forgejo.py
+++ simplex-alerter/simplex_alerter/webhook/request_models/forgejo.py
@@ -1,3 +1,26 @@
 from pydantic import BaseModel
 from typing import Union
-from jinja2 import Environment
+from jinja2 import Template
+
+class PushNotification(BaseModel):
+    refs: str
+    before: str
+    after: str
+    compare_url: str
+    commits: list
+    total_commits: int
+    head_commit: dict
+    repository: dict
+    pusher: dict
+    sender: dict
+    template: str = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.template = Template("""
+New content pushed to {{repository["full_name"}}!
+
+{{head_commit["message"]}}""", enable_async = True)
+
+    async def render(self):
+        return await self.template.render_async(self.model_dump())
