704c02eba5c9cca8cfb82722a6e5c95a727bef2a
midas <dev@null>
2025-06-10 12:33:40 +0200

    patch servarr template

--- simplex-alerter/simplex_alerter/webhook/request_models/servarr.py
+++ simplex-alerter/simplex_alerter/webhook/request_models/servarr.py
@@ -3,16 +3,19 @@ from pydantic import BaseModel
 from typing import Union, Optional
 
 class SonarrAlert(BaseModel):
-    series: dict
-    episodes: list
     eventType: str
+    source: Optional[str] = None
+    host: Optional[str] = None
+    series: Optional[dict]
+    episodes: Optional[list]
+    release: Optional[dict] = None
     instanceName: Optional[str] = None
+    trigger: Optional[str] = None
     applicationUrl: Optional[str] = None
     customFormatInfo: Optional[dict] = None
     downloadClient: Optional[str] = None
     downloadClientType: Optional[str] = None
     downloadId: Optional[str] = None
-    release: Optional[dict] = None
     template: str = None
 
 
@@ -28,6 +31,10 @@ class SonarrAlert(BaseModel):
 {{i["remoteUrl"]}}
     {% endif %}
 {% endfor %}
+
+{% if release != None %}
+{{ release.releaseTitle}} found on {{ release.indexer }}
+{% endif %}
 
 {% for ep in episodes %}
 - S{{ep["seasonNumber"]}}E{{ep["episodeNumber"]}}
