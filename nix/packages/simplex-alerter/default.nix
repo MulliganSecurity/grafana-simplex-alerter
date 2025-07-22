66bfa277f0932b98875bc8f622136b0707e28357
midas <dev@null>
2025-07-22 09:33:38 +0200

    add build directives for simplex-chat autonomous init

--- nix/packages/simplex-alerter/default.nix
+++ nix/packages/simplex-alerter/default.nix
@@ -18,5 +18,36 @@ let
     inherit inputs lib pkgs;
     package_name = namespace;
   };
+  simplex-chat = pkgs.stdenv.mkDerivation {
+    name = "simplex-chat";
+    version = "v6.3.4";
+    src = pkgs.fetchurl {
+      url = "https://github.com/simplex-chat/simplex-chat/releases/download/v6.3.4/simplex-chat-ubuntu-24_04-x86-64";
+      hash = "sha256-8A2jqRaRYy7okGDD8Q8Gx7ZttxXhcSDsFRKvvdbyZHc=";
+    };
+    dontBuild = true;
+    dontUnpack = true;
+    installPhase = "mkdir -p $out/bin; cp $src $out/bin/simplex-chat; chmod +x $out/bin/simplex-chat";
+  };
+
+  externalDeps = with pkgs; [
+    simplex-chat
+  ];
+
 in
-common.pythonSet.mkVirtualEnv common.package_name common.workspace.deps.default
+  (common.pythonSet.mkVirtualEnv common.package_name common.workspace.deps.default).overrideAttrs (
+    old: {
+  nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+  buildInputs = (old.buildInputs or []) ++ externalDeps;
+  
+  postFixup = (old.postFixup or "") + ''
+    for exe in $out/bin/*; do
+      if [[ -f "$exe" && -x "$exe" ]]; then
+        wrapProgram "$exe" \
+          --prefix PATH : ${lib.makeBinPath externalDeps}
+      fi
+    done
+'';
+    }
+
+  )
